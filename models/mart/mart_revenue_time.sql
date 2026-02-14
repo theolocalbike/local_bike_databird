42f7b7b1cbaa5aaf9bf7264ac7a463a63e611890
bachelardtheo74@gmail.com <bachelardtheo74@gmail.com>
2026-02-14 16:07:29 +0000

    feature mart models

---
+++ models/mart/mart_revenue_time.sql
@@ -0,0 +1,29 @@
+-- models/marts/mart_revenue_time.sql
+
+with sales as (
+
+    select *
+    from {{ ref('int_sales_enriched') }}
+
+)
+
+select
+
+    order_date,
+    order_year,
+    order_month,
+
+    count(distinct order_id) as total_orders,
+    count(distinct customer_id) as total_customers,
+
+    sum(quantity) as total_units_sold,
+    sum(net_revenue) as net_revenue,
+    sum(discount_amount) as total_discount_given,
+
+    sum(net_revenue) / nullif(count(distinct order_id), 0) as avg_order_value
+
+from sales
+group by
+    order_date,
+    order_year,
+    order_month
---
+++ models/mart/mart_store_performance.sql
@@ -0,0 +1,35 @@
+-- models/marts/mart_store_performance.sql
+
+with sales as (
+
+    select *
+    from {{ ref('int_sales_enriched') }}
+
+)
+
+select
+
+    store_id,
+    store_name,
+    store_city,
+    store_state,
+
+    count(distinct order_id) as total_orders,
+    count(distinct customer_id) as total_customers,
+
+    sum(quantity) as total_units_sold,
+
+    sum(gross_revenue) as gross_revenue,
+    sum(net_revenue) as net_revenue,
+    sum(discount_amount) as total_discount_given,
+
+    sum(net_revenue) / nullif(count(distinct order_id), 0) as avg_order_value,
+
+    sum(net_revenue) / nullif(count(distinct customer_id), 0) as revenue_per_customer
+
+from sales
+group by
+    store_id,
+    store_name,
+    store_city,
+    store_state
